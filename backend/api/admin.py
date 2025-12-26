9038ee0b2681ca2265f3c1a3df37f7d138b64f51
Viom <viomstha2005@gmail.com>
2025-12-26 15:56:06 +0545

    Added color extraction and some features

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import StorageUnit, ClothingItem, Outfit
 
-# Register your models here.
+admin.site.register(StorageUnit)
+admin.site.register(ClothingItem)
+admin.site.register(Outfit)
+
+
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,14 +1,34 @@
 from django.urls import path
-from .views import RegisterView,profile,admin_dashboard,segment_clothing, delete_segmented_image
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = [
-    path('register/', RegisterView.as_view()),
-    path('login/', TokenObtainPairView.as_view()),
-    path('profile/', profile),
-    path('refresh/', TokenRefreshView.as_view()),
-    path('admin-dashboard/', admin_dashboard),
-    path('segment/', segment_clothing),
-    path('delete-segmented/', delete_segmented_image), 
-]
+from .views import (
+    RegisterView, profile, admin_dashboard, 
+    segment_clothing, save_clothing_item,
+    delete_segmented_image, logout,list_storage_units,
+    save_non_clothing_item, list_non_clothing_items, extract_metadata, authenticate_clothing
+)
 
+urlpatterns = [
+    # Auth
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', logout, name='logout'),
+
+    # Profile
+    path('profile/', profile, name='profile'),
+
+    # Admin
+    path('admin-dashboard/', admin_dashboard, name='admin_dashboard'),
+
+    # Clothing item
+    path('segment/', segment_clothing, name='segment_clothing'),
+    path('save-clothing/', save_clothing_item, name='save_clothing_item'),
+    path('delete-segmented/', delete_segmented_image, name='delete_segmented_image'),
+    path('storage/', list_storage_units, name='list_storage_units'),
+    path('non-clothing/', save_non_clothing_item, name='save_non_clothing_item'),
+    path('non-clothing/list/', list_non_clothing_items, name='list_non_clothing_items'),
+    path('ai/authenticate-clothing/', authenticate_clothing, name='authenticate_clothing'),
+    path('ai/extract-metadata/', extract_metadata, name='extract_metadata'),
+
+]
